d6f2b247e96db58e79a745d7b1e9205bdd838554
yhames <wjddn494@naver.com>
2025-03-13 20:10:47 +0900

    Register authentication routes with '/auth' prefix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import userRoute from './user';
+import authRoute from './auth';
 
 const routes = async (fastify: FastifyInstance) => {
+  fastify.register(authRoute, { prefix: '/auth' });
   fastify.register(userRoute, { prefix: '/user' });
 };
 
